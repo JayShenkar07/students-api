a1c51f21b9c13ac2c1773a270f25af73b38c81f5
JayShenkar07 <jayshenkar27apr@gmail.com>
2025-01-05 23:22:43 +0530

    added basic server and config setups

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,6 +1,47 @@
 package main
-import "fmt"
 
-func main(){
-	fmt.Println("Welcome to Students API")
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/JayShenkar07/students-api/internal/config"
+)
+
+func main() {
+	fmt.Println("Hello")
+
+	// Load configuration
+	cfg := config.MustLoad()
+
+	// // Print loaded config values for confirmation
+	// fmt.Printf("Running in environment: %s\n", cfg.Env)
+	// fmt.Printf("Storage Path: %s\n", cfg.Storage)
+	fmt.Printf("HTTP Server Address: %s\n", cfg.Addr)
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /api/student/home", userHome)
+	router.HandleFunc("GET /api/student/profile", userProfile)
+	
+	server := http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Println("Server Started!")
+	err := server.ListenAndServe()
+	if err != nil{
+		log.Fatal("Failed to start server!")
+	}
+	
+}
+
+func userHome(writer http.ResponseWriter, request *http.Request) {
+	var message string = "api/student/home"
+	fmt.Fprint(writer, message)
+}
+
+func userProfile(writer http.ResponseWriter, request *http.Request) {
+	var message string = "api/student/profile"
+	fmt.Fprint(writer, message)
 }
